2e57f830bf02fab49e5ae21b8ec0fa09db44a81b
Nick Gromyko <nxgr.dev@gmail.com>
2023-04-23 22:28:42 +0600

    update(src/components/PxToRem.jsx): section logic

--- src/components/PxToRem.jsx
+++ src/components/PxToRem.jsx
@@ -2,20 +2,73 @@ import WInput from "./shared/input/WInput.jsx";
 import {useState} from "react";
 
 function PxToRem() {
-    const [value, setValue] = useState('')
+    const [pxValue, setPxValue] = useState(0)
+    const [remValue, setRemValue] = useState("")
+    const [baseValue, setBaseValue] = useState(16)
 
-    function handleChange(e) {
-        setValue(e.target.value)
+    function handlePxChange(event) {
+        const value = event.target.value
+
+        setPxValue(value)
+        if (+value) {
+            setRemValue(calcValues(+value, null, +baseValue))
+        }
+    }
+
+    function handleRemChange(event) {
+        const value = event.target.value
+
+        setRemValue(value)
+        if (+value) {
+            setPxValue(calcValues(null, +value, +baseValue))
+        }
+    }
+
+    function handleBaseChange(event) {
+        const value = event.target.value
+
+        setBaseValue(value)
+        if (+value) {
+            setRemValue(calcValues(+pxValue, null, +value))
+        }
+    }
+
+    function calcValues(px, rem, base) {
+        if (typeof px === "number") {
+            if (base === 0) {
+                return 0
+            }
+            return px / base
+        }
+        if (typeof rem === "number") {
+            return rem * base
+        }
     }
 
     return (
         <div className="section-card">
             <div className="section-title">Px to rem</div>
-            <WInput
-                value={value}
-                onChange={handleChange}
-                type="number"
-            />
+            <div className="section_px-to-rem_flex">
+                <WInput
+                    value={pxValue}
+                    onChange={handlePxChange}
+                    type="number"
+                    appendText="px"
+                />
+                <img src="../../public/swap-horizontal.svg" alt=""/>
+                <WInput
+                    value={remValue}
+                    onChange={handleRemChange}
+                    type="number"
+                    appendText="rem"
+                />
+                <WInput
+                    value={baseValue}
+                    onChange={handleBaseChange}
+                    type="number"
+                    appendText="base"
+                />
+            </div>
         </div>
     )
 }
